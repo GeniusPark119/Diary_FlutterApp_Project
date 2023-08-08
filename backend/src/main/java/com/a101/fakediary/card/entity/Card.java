c9cdd9d2b1893f84bd0290541303e9a80e0d246d
jwelyl <cork2586@naver.com>
2023-08-08 18:22:07 +0900

    🛠 Fix : card 테이블에 card_style_id으로 index 추가
    Consist of Card.java
    
    RESOLVE : #S08P3A101-868

--- backend/src/main/java/com/a101/fakediary/card/entity/Card.java
+++ backend/src/main/java/com/a101/fakediary/card/entity/Card.java
@@ -14,6 +14,7 @@ import java.math.BigDecimal;
 @AllArgsConstructor
 @Builder
 @Entity
+@Table(name = "card", indexes =  {@Index(name = "idx__style", columnList = "card_style_id")})
 public class Card extends BaseTimeEntity {
     @SequenceGenerator(
             name = "CARD_SEQ_GEN",
