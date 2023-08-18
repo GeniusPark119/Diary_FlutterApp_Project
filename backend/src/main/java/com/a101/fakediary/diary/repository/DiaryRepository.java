8b00b74cf9b5ec2beec4bd8857d94c5c3601b6ab
jwelyl <cork2586@naver.com>
2023-08-18 21:12:17 +0900

    🛠 Fix : 불필요한 import 제거
    Consist of DiaryRepository.java
    
    RESOLVE : #S08P3A101-868

--- backend/src/main/java/com/a101/fakediary/diary/repository/DiaryRepository.java
+++ backend/src/main/java/com/a101/fakediary/diary/repository/DiaryRepository.java
@@ -10,7 +10,6 @@ import org.springframework.stereotype.Repository;
 
 import javax.transaction.Transactional;
 import java.util.List;
-import java.util.Optional;
 
 @Repository
 public interface DiaryRepository extends JpaRepository<Diary, Long> {
