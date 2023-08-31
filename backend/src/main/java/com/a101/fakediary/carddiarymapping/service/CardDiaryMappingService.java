259dc5cbfc32e79fdfd6865edf674c1d3a53204b
Jae-Seong Heo <cork2586@naver.com>
2023-09-01 00:54:41 +0900

    🛠 Fix : CardDiaryMappingService 수정
    Consist of CardDiaryMappingService.java
    
    RESOLVE : #S08P3A101-868

--- backend/src/main/java/com/a101/fakediary/carddiarymapping/service/CardDiaryMappingService.java
+++ backend/src/main/java/com/a101/fakediary/carddiarymapping/service/CardDiaryMappingService.java
@@ -25,7 +25,6 @@ import java.util.List;
 public class CardDiaryMappingService {
 
     private final CardDiaryMappingRepository cardDiaryMappingRepository;
-    private final CardService cardService;
     private final CardRepository cardRepository;
     private final DiaryRepository diaryRepository;
 
